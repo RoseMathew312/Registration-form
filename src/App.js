6a2974700344b4a3aadc2f4676be1408a466bf45
unknown <admin@RVSKCH215LT.ADRAPIDVALUE.COM>
2021-06-10 16:18:44 +0530

    changes in prettier settings

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React from 'react';
-import './App.css';
-import Button from './Components/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAppleAlt } from '@fortawesome/free-solid-svg-icons/faAppleAlt'
-import { faEye} from '@fortawesome/free-solid-svg-icons/faEye'
+import React from "react";
+import "./App.css";
+import Button from "./Components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAppleAlt } from "@fortawesome/free-solid-svg-icons/faAppleAlt";
+import { faEye } from "@fortawesome/free-solid-svg-icons/faEye";
 function App() {
   return (
-<div>
-<Button primary>Primary Button</Button>
-			<Button outline color="blue">
-				Outline Button
-			</Button>
-			<Button pill color="red">
-				Pill Button
-			</Button>
-			<Button square color="black">
-				Square Button
-			</Button>
-			<Button disabled>Disabled Button</Button>
+    <div>
+      <Button primary>Primary Button</Button>
+      <Button outline color="blue">
+        Outline Button
+      </Button>
+      <Button pill color="red">
+        Pill Button
+      </Button>
+      <Button square color="black">
+        Square Button
+      </Button>
+      <Button disabled>Disabled Button</Button>
       <Button>
-				<FontAwesomeIcon icon={faAppleAlt} />Button
-			</Button>
+        <FontAwesomeIcon icon={faAppleAlt} />
+        Button
+      </Button>
       <Button pill>
-				<FontAwesomeIcon icon={faEye} />Button
-			</Button>
-</div>
-
-
-
-
+        <FontAwesomeIcon icon={faEye} />
+        Button
+      </Button>
+    </div>
   );
-
-
-
-
-
 }
 
 export default App;
-
