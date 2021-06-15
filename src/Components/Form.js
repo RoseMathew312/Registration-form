05e141020c8cfd74c8af76eb7c0e6e314f107497
unknown <admin@RVSKCH215LT.ADRAPIDVALUE.COM>
2021-06-15 18:26:46 +0530

    Recreate the form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,24 +1,12 @@
-import React, { useState } from 'react';
-import './Form.css';
-import FormSignup from './FormSignup';
-import FormSuccess from './FormSuccess';
+import React from "react";
+import "./Form.css";
+import FormSignup from "./FormSignup";
 
 const Form = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  function submitForm() {
-    setIsSubmitted(true);
-  }
   return (
     <>
-      <div className='form-container'>
-        <span className='close-btn'>×</span>
-       
-        {!isSubmitted ? (
-          <FormSignup submitForm={submitForm} />
-        ) : (
-          <FormSuccess />
-        )}
+      <div className="form-container">
+        <FormSignup />
       </div>
     </>
   );
